9bc50bb3216e92367e141e03a18372058ce0e330
SagarDesai10 <desaisagar8090@gmail.com>
2025-03-02 00:27:13 +0530

    add zipkin and micrometer

---
+++ src/test/java/com/zipkin/demo/ZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipkin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
